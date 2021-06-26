f51500f0f23313b9d1f8a03e20688313df462cd4
zulkan <m.iqbal.z.n@gmail.com>
2021-06-26 20:25:37 +0700

    update sabtu debug

--- app/main.go
+++ app/main.go
@@ -6,8 +6,32 @@ import (
 	"net/http"
 )
 
+func filterIP(req *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+
+	log.Println("host: ", req.RemoteAddr)
+
+	return req, nil
+}
+
+type handleConnect struct {
+}
+
+func (h handleConnect) HandleConnect(host string, ctx *goproxy.ProxyCtx) (*goproxy.ConnectAction, string) {
+	log.Println("CONNECT REQ", host, " FROM ", ctx.Req.RemoteAddr)
+
+	return goproxy.OkConnect, host
+}
+
+func getHandleConnect() goproxy.HttpsHandler {
+	return handleConnect{}
+}
+
 func main() {
 	proxy := goproxy.NewProxyHttpServer()
 	proxy.Verbose = true
-	log.Fatal(http.ListenAndServe(":8080", proxy))
+
+	proxy.OnRequest().DoFunc(filterIP)
+	proxy.OnRequest().HandleConnect(getHandleConnect())
+
+	log.Fatal(http.ListenAndServe(":8181", proxy))
 }
